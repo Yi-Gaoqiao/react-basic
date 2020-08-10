fcfefe90280cfd23cf9b680b8acd6e5feffff6c7
Tsubasa Takahashi <2bsttas@gmail.com>
2020-08-11 08:47:35 +0900

    create Basic_2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Basic_1 from './components/Basic_1'
+import Basic_2 from './components/Basic_2'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Basic_1 name="Hooks"/>
-        <Basic_1 name="Today"/>
-
+        <Basic_2 />
+        
       </header>
     </div>
   );
