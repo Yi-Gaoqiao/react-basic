6eae0ec2c04a88342591301b2ee4f9647b5ecbaf
Tsubasa Takahashi <2bsttas@gmail.com>
2020-08-11 10:03:32 +0900

    add BasicUseEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Basic_1 from './components/Basic_1'
-import Basic_2 from './components/Basic_2'
+// import Basic_1 from './components/Basic_1'
+// import Basic_2 from './components/Basic_2'
+import BasicUseEffect from './components/BasicUseEffect'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Basic_2 />
+        {/* <Basic_2 /> */}
+        <BasicUseEffect />
         
       </header>
     </div>
